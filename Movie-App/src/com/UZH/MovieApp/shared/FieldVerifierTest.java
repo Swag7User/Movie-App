1c403848ed4bc48f90f40183a1624ad25907d037
SandroLuck <sandro.luck@gmx.ch>
2015-11-09 17:44:06 +0100

    ty

---
+++ Movie-App/src/com/UZH/MovieApp/shared/FieldVerifierTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.UZH.MovieApp.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Sandro
+ *
+ */
+public class FieldVerifierTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
