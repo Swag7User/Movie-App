f8050edf3582e0d88fbea73439c16974e5f4aa8b
Swag7User <oertle.d@gmail.com>
2015-12-07 17:33:27 +0100

    mysqlaccess

--- Movie-App/src/com/UZH/MovieApp/server/MySQLAccessTest.java
+++ Movie-App/src/com/UZH/MovieApp/server/MySQLAccessTest.java
@@ -20,7 +20,19 @@ public class MySQLAccessTest {
                 k = null;
                e.printStackTrace();
            }
-           assertEquals(k, null);
+           Movie testMovie1 = k.get(0);
+           Movie testMovie2 = new Movie("330", "/m/0ktn59", "Actrius", "1996", "0", "90.0", "Catalan, Spanish", "Spain", "Drama, Comedy-drama");
+           assertEquals(testMovie1.getWikiid(), testMovie2.getWikiid());
+           assertEquals(testMovie1.getFreebaseid(), testMovie2.getFreebaseid());
+           assertEquals(testMovie1.getName(), testMovie2.getName());
+           assertEquals(testMovie1.getReleasedate(), testMovie2.getReleasedate());
+           assertEquals(testMovie1.getBoxoffice(), testMovie2.getBoxoffice());
+           assertEquals(testMovie1.getRuntime(), testMovie2.getRuntime());
+           assertEquals(testMovie1.getLanguages(), testMovie2.getLanguages());
+           assertEquals(testMovie1.getCountries(), testMovie2.getCountries());
+           assertEquals(testMovie1.getGenres(), testMovie2.getGenres());
+
+
        }
     }
 
